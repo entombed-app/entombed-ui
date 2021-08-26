9fbcb43efd31cf5aca7309359bc285afe3471557
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-26 18:56:55 -0500

    Add initial app setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
+import { Switch, NavLink, Link, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <Header />
+      <Switch>
+        <Route exact from='/'>
+          <section className='window'>
+            <NavLink exact to='/countdown'>Countdown</NavLink>
+            <NavLink exact to='/obituary'>Obituary</NavLink>
+            <NavLink exact to='/timeline'>Timeline</NavLink>
+            <NavLink exact to='/relations'>Relations</NavLink>
+            <NavLink exact to='/gallery'>Gallery</NavLink>
+            <div className='pane'>
+                Placeholder
+            </div>
+            <div className='pane'>
+                Placeholder
+            </div>
+            <div className='pane'>
+                Placeholder
+            </div>
+          </section>
+        </Route>
+      </Switch>
     </main>
   );
 }
