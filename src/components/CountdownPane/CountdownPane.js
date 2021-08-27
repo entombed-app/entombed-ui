1a075fed2d9a81bf60b7630b3b81295b3c8bdf14
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-27 14:18:15 -0600

    Build basic structure of CountdownPane sundial

---
+++ src/components/CountdownPane/CountdownPane.js
@@ -0,0 +1,13 @@
+import "./CountdownPane.css"
+
+export const CountdownPane = props => {
+  return (
+    <>
+    <section className="sundial">
+      <div className="circle">
+
+      </div>
+    </section>
+    </>
+  )
+}
