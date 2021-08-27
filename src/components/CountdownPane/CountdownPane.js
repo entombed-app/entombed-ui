c1b83594b7387596fadd0c9b9dcd97b63ec005f2
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-27 16:55:08 -0600

    Move marks to outer circle edge and refactor their borders

--- src/components/CountdownPane/CountdownPane.js
+++ src/components/CountdownPane/CountdownPane.js
@@ -4,20 +4,22 @@ export const CountdownPane = () => {
   return (
     <>
     <section className="sundial">
-      <div className="circle">
-        <div className="marks">
-          <div className="wedge"></div>
-          <div className="mark">I</div>
-          <div className="mark">II</div>
-          <div className="mark">III</div>
-          <div className="mark">IV</div>
-          <div className="mark">V</div>
-          <div className="mark">VI</div>
-          <div className="mark">VII</div>
-          <div className="mark">VIII</div>
-          <div className="mark">IX</div>
-          <div className="mark">X</div>
-          <div className="mark">XI</div>
+      <div className="big-circle">
+        <div className="circle">
+          <div className="marks">
+            <div className="wedge"></div>
+            <div className="mark">I</div>
+            <div className="mark">II</div>
+            <div className="mark">III</div>
+            <div className="mark">IV</div>
+            <div className="mark">V</div>
+            <div className="mark">VI</div>
+            <div className="mark">VII</div>
+            <div className="mark">VIII</div>
+            <div className="mark">IX</div>
+            <div className="mark">X</div>
+            <div className="mark">XI</div>
+          </div>
         </div>
       </div>
     </section>
