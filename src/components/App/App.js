c5a75eb0bc6c0898a5e820b2dc7597eded9d7384
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-26 19:53:58 -0500

    Add initial dashboard grid layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,15 +12,15 @@ const App = () => {
           <section className='window'>
             <NavLink className='preview-pane' exact to='/preview'>Preview</NavLink>
             <NavLink className='countdown-pane' exact to='/countdown'>Countdown</NavLink>
-            <NavLink className='obit-pane' exact to='/obituary'>Obituary</NavLink>
-            <NavLink className='timeline-pane' exact to='/timeline'>Timeline</NavLink>
             <NavLink className='executor-pane' exact to='/executors'>Executors</NavLink>
-            <NavLink className='recipient-pane' exact to='/recipients'>Recipients</NavLink>
-            <NavLink className='gallery-pane' exact to='/gallery'>Gallery</NavLink>
-            <div className='pane'>
+            <div className='placeholder-1'>
                 Placeholder
             </div>
-            <div className='pane'>
+            <NavLink className='obit-pane' exact to='/obituary'>Obituary</NavLink>
+            <NavLink className='timeline-pane' exact to='/timeline'>Timeline</NavLink>
+            <NavLink className='recipient-pane' exact to='/recipients'>Recipients</NavLink>
+            <NavLink className='gallery-pane' exact to='/gallery'>Gallery</NavLink>
+            <div className='placeholder-2'>
                 Placeholder
             </div>
           </section>
