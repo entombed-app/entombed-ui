c6f621f8f77509d4f215a5c905e78919d06a18db
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-26 19:22:41 -0500

    Add remaining nav links

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,14 +10,13 @@ const App = () => {
       <Switch>
         <Route exact from='/'>
           <section className='window'>
-            <NavLink exact to='/countdown'>Countdown</NavLink>
-            <NavLink exact to='/obituary'>Obituary</NavLink>
-            <NavLink exact to='/timeline'>Timeline</NavLink>
-            <NavLink exact to='/relations'>Relations</NavLink>
-            <NavLink exact to='/gallery'>Gallery</NavLink>
-            <div className='pane'>
-                Placeholder
-            </div>
+            <NavLink className='preview-pane' exact to='/preview'>Preview</NavLink>
+            <NavLink className='countdown-pane' exact to='/countdown'>Countdown</NavLink>
+            <NavLink className='obit-pane' exact to='/obituary'>Obituary</NavLink>
+            <NavLink className='timeline-pane' exact to='/timeline'>Timeline</NavLink>
+            <NavLink className='executor-pane' exact to='/executors'>Executors</NavLink>
+            <NavLink className='recipient-pane' exact to='/recipients'>Recipients</NavLink>
+            <NavLink className='gallery-pane' exact to='/gallery'>Gallery</NavLink>
             <div className='pane'>
                 Placeholder
             </div>
