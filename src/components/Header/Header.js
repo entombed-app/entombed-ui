4db874f538dc4824a97e7f6de8915ada04fe5323
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-28 15:37:57 -0500

    Convert to REM

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import userPicture from '../../assets/user.png';
 
 const Header = () => {
     return (
         <header className='header'>
-            <div className='sun'></div>
+            <div className='sun'><img src={userPicture}/></div>
             <h1 className='title'>Elegy</h1>
         </header>
     )
