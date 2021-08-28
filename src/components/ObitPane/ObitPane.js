3e2e7ef2a2a4ad85636018d9af1778b15e5551b0
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-28 17:29:09 -0500

    Add basic JSX for obit pane

---
+++ src/components/ObitPane/ObitPane.js
@@ -0,0 +1,11 @@
+import './ObitPane.css'
+
+const ObitPane = () => {
+    return (
+        <section className='obit'>
+            <div></div>
+            <article>I was a good boy, a nice friend, and good at sharing</article>
+            <button>Edit</button>
+        </section>
+    )
+}
