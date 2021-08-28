c391d15870aa73cde7db49e0741ee611c41010b5
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-28 14:59:18 -0600

    Build outer and inner progress divs to track days left to release

--- src/components/CountdownPane/CountdownPane.js
+++ src/components/CountdownPane/CountdownPane.js
@@ -34,24 +34,28 @@ export const CountdownPane = ({est, err}) => {
       <section className="release-days">
         {error.length ? <h2>{error}</h2> : <h2> Days until release: {timeLeft.days}</h2>}
       </section>
-      <div className="big-circle">
-        <div className="circle">
-          <div className="profile">
-            <img className="prof-photo" src={sunface} />
-          </div>
-          <div className="marks">
-            <div className="wedge"></div>
-            <div className="mark">I</div>
-            <div className="mark">II</div>
-            <div className="mark">III</div>
-            <div className="mark">IV</div>
-            <div className="mark">V</div>
-            <div className="mark">VI</div>
-            <div className="mark">VII</div>
-            <div className="mark">VIII</div>
-            <div className="mark">IX</div>
-            <div className="mark">X</div>
-            <div className="mark">XI</div>
+      <div className="progress-outer">
+        <div className="progress-inner">
+          <div className="big-circle">
+            <div className="circle">
+              <div className="profile">
+                <img className="prof-photo" src={sunface} />
+              </div>
+              <div className="marks">
+                <div className="wedge"></div>
+                <div className="mark">I</div>
+                <div className="mark">II</div>
+                <div className="mark">III</div>
+                <div className="mark">IV</div>
+                <div className="mark">V</div>
+                <div className="mark">VI</div>
+                <div className="mark">VII</div>
+                <div className="mark">VIII</div>
+                <div className="mark">IX</div>
+                <div className="mark">X</div>
+                <div className="mark">XI</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
