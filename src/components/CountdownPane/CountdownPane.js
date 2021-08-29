e138cfa0516d50c9a72b944029622bf1a08d348c
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-28 20:11:55 -0600

    Place svg and circle in CountdownPane to prepare for eventual animation

--- src/components/CountdownPane/CountdownPane.js
+++ src/components/CountdownPane/CountdownPane.js
@@ -24,42 +24,65 @@ export const CountdownPane = ({est, err}) => {
         seconds: Math.floor((difference / 1000) % 60)
       }
     }
-    console.log(updatedTimeLeft)
     setTimeLeft(updatedTimeLeft)
   }
 
   return (
-    <>
-    <section className="sundial">
+    <section className="sundial-display">
       <section className="release-days">
-        {error.length ? <h2>{error}</h2> : <h2> Days until release: {timeLeft.days}</h2>}
+          {error.length ? <h2>{error}</h2> : <h2> Days until release: {timeLeft.days}</h2>}
       </section>
-      <div className="progress-outer">
-        <div className="progress-inner">
-          <div className="big-circle">
-            <div className="circle">
-              <div className="profile">
-                <img className="prof-photo" src={sunface} />
-              </div>
-              <div className="marks">
-                <div className="wedge"></div>
-                <div className="mark">I</div>
-                <div className="mark">II</div>
-                <div className="mark">III</div>
-                <div className="mark">IV</div>
-                <div className="mark">V</div>
-                <div className="mark">VI</div>
-                <div className="mark">VII</div>
-                <div className="mark">VIII</div>
-                <div className="mark">IX</div>
-                <div className="mark">X</div>
-                <div className="mark">XI</div>
+      <section className="sundial">
+        {/* <div className="progress-outer">
+          <div className="progress-inner"> */}
+          <svg 
+            className="progress"
+          >
+              <circle
+                className="progress-ring"
+                // stroke={"#FFFFFF"}
+                // strokeWidth={4}
+                // fill="transparent"
+                r="27vh"
+                cx="25vh"
+                cy="25vh"
+              >
+              </circle>
+          </svg>
+            <div className="big-circle">
+              <div className="circle">
+                <div className="profile">
+                  <img className="prof-photo" src={sunface} />
+                </div>
+                <div className="marks">
+                  {/* <div className="wedge"></div> */}
+                  <div className="mark">XII</div>
+                  <div className="mark">I</div>
+                  <div className="mark">II</div>
+                  <div className="mark">III</div>
+                  <div className="mark">IV</div>
+                  <div className="mark">V</div>
+                  <div className="mark">VI</div>
+                  <div className="mark">VII</div>
+                  <div className="mark">VIII</div>
+                  <div className="mark">IX</div>
+                  <div className="mark">X</div>
+                  <div className="mark">XI</div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+            {/* </div> */}
+            {/* <div className="outer">
+              <div className="bar left">
+                <div className="progress"></div>
+              </div>
+              <div className="bar right">
+                <div className="progress"></div>
+              </div>
+            </div> */}
+          {/* </div>
+        </div> */}
+      </section>
     </section>
-    </>
   )
 }
