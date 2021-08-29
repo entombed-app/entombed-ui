4e8fe36c22d050829188afc519f06680c107533a
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-29 11:51:07 -0500

    Add divs for panes surrounding obit

--- src/components/ObitPane/ObitPane.js
+++ src/components/ObitPane/ObitPane.js
@@ -4,7 +4,22 @@ import './ObitPane.css';
 const ObitPane = () => {
     return (
         <section className='obit'>
-            <div className='triangle'></div>
+            <div className='small-square'></div>
+            <div className='rectangle'></div>
+            <div className='small-square'></div>
+            <div className='rectangle'></div>
+            <div className='small-square'></div>
+            <div className='rectangle'></div>
+            <div className='rectangle'></div>
+            <div className='small-square'></div>
+            <div className='small-square'></div>
+            <div className='rectangle'></div>
+            <div className='rectangle'></div>
+            <div className='small-square'></div>
+            <div className='rectangle'></div>
+            <div className='small-square'></div>
+            <div className='rectangle'></div>
+            <div className='small-square'></div>
             <article className='obit-text'>I was a good boy, a nice friend, and good at sharing</article>
             <button className='edit-button'>Edit</button>
         </section>
