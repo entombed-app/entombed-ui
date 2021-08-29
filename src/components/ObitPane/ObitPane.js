1dbe9489796caecc2b40d1f51f3575692659c88d
Taylor Galloway <gallowaytaylor@outlook.com>
2021-08-29 11:59:52 -0500

    Add small-square grid placement

--- src/components/ObitPane/ObitPane.js
+++ src/components/ObitPane/ObitPane.js
@@ -4,22 +4,22 @@ import './ObitPane.css';
 const ObitPane = () => {
     return (
         <section className='obit'>
-            <div className='small-square'></div>
+            <div className='small-square corner1'></div>
             <div className='rectangle'></div>
-            <div className='small-square'></div>
+            <div className='small-square middle1'></div>
             <div className='rectangle'></div>
-            <div className='small-square'></div>
-            <div className='rectangle'></div>
-            <div className='rectangle'></div>
-            <div className='small-square'></div>
-            <div className='small-square'></div>
+            <div className='small-square corner2'></div>
             <div className='rectangle'></div>
             <div className='rectangle'></div>
-            <div className='small-square'></div>
+            <div className='small-square middle2'></div>
+            <div className='small-square middle3'></div>
             <div className='rectangle'></div>
-            <div className='small-square'></div>
             <div className='rectangle'></div>
-            <div className='small-square'></div>
+            <div className='small-square corner3'></div>
+            <div className='rectangle'></div>
+            <div className='small-square middle4'></div>
+            <div className='rectangle'></div>
+            <div className='small-square corner4'></div>
             <article className='obit-text'>I was a good boy, a nice friend, and good at sharing</article>
             <button className='edit-button'>Edit</button>
         </section>
