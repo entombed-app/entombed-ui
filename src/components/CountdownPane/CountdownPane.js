7d80d5b19ef25979c6f23dab59b43a045eb2bc6e
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-08-29 13:18:14 -0600

    Fix percentage calculation for progress bar

--- src/components/CountdownPane/CountdownPane.js
+++ src/components/CountdownPane/CountdownPane.js
@@ -4,8 +4,9 @@ import React, { useEffect, useState } from "react";
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-export const CountdownPane = ({est, err}) => {
-  const [etd, setEtd] = useState(est)
+export const CountdownPane = ({etd, err, dob}) => {
+  const [etdeath, setEtd] = useState(etd)
+  const [dobirth] = useState(dob)
   const [timeLeft, setTimeLeft] = useState(0)
   const [percentage, setPercentage] = useState(0)
   const [error] = useState(err)
@@ -17,14 +18,13 @@ export const CountdownPane = ({est, err}) => {
   const calculateTimeLeft = () => {
     let newYear, newEtd
     const splitEtd = etd.split("/")
-    if (splitEtd[2].length < 4) {
-      newYear = Number(`21${splitEtd[2]}`) + 122
-      newEtd = `${splitEtd[0]}/${splitEtd[1]}/${newYear}`
+
+      newEtd = `${splitEtd[0]}/${splitEtd[1]}/${splitEtd[2]}`
       setEtd(newEtd)
       calculateProgress()
-    }
 
     const difference = +new Date(`${newEtd}`) - +new Date()
+
     let updatedTimeLeft = {}
   
     if (difference > 0) {
@@ -39,9 +39,14 @@ export const CountdownPane = ({est, err}) => {
   }
 
   const calculateProgress = () => {
-    const total = +new Date(`${etd}`)
-    const perc = total / +new Date
-    setPercentage(perc * 100)
+    const deathDay = +new Date(`${etd}`)
+    const birthDay = +new Date(`${dob}`)
+    const totalLife = deathDay - birthDay
+    const today =  +new Date()
+    const livedTime = today - birthDay
+    const perc = livedTime / totalLife
+
+    setPercentage((1 - perc) * 100)
   }
 
   return (
