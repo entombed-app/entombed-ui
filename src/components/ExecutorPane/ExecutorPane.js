633864337311c5dc5d86c6a37fce803a8fdfe6e8
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-01 14:08:50 -0600

    Build exec window panes shapes and lines

--- src/components/ExecutorPane/ExecutorPane.js
+++ src/components/ExecutorPane/ExecutorPane.js
@@ -3,7 +3,38 @@ import React from "react"
 export const ExecutorPane = () => {
   return (
     <section className="executor-pane">
+      <section className="exec-border">
+        <h2>Executor</h2>
+        <div className="exec-window">
+          <div className="shape one">
+          </div>
+          <div className="line l-one">
+          </div>
+          <div className="shape two">
+          </div>
+          <div className="line l-two">
+          </div>
+          <div className="shape three">
+          </div>
+          <div className="line l-three">
+          </div>
+          <div className="shape four">
+          </div>
+          <div className="line l-four">
+          </div>
+          <div className="shape five">
+          </div>
+          <div className="line l-five">
+          </div>
+          <div className="shape six">
+          </div>
+          <div className="shape seven">
+          </div>
+          <div className="line l-six">
+          </div>
+        </div>
 
+      </section>
     </section>
   )
 }
