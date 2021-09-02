fd8429b2abbb0320aba79dc3000a0dc51750a0bf
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-02 15:09:27 -0600

    Fix merge conflict

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,17 +6,17 @@ const Header = () => {
     return (
         <header className='header'>
             <div className='sun'><img src={userPicture}/></div>
-            <section class="title-diamond">
-                <div class="header-diamond left"></div>
-                <div class="header-diamond center"></div>
+            <section className="title-diamond">
+                <div className="header-diamond left"></div>
+                <div className="header-diamond center"></div>
             {/* <div class="header-stone center"></div> */}
-                <div class="header-diamond right"></div>
+                <div className="header-diamond right"></div>
                 <h1>Elegy</h1>
             </section>
-            <div class="stripes">
-            <div class="stripe"></div>
-            <div class="stripe"></div>
-            <div class="stripe"></div>
+            <div className="stripes">
+            <div className="stripe"></div>
+            <div className="stripe"></div>
+            <div className="stripe"></div>
             </div>
         </header>
     )
