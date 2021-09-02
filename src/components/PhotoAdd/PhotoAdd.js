89a33f6c6f266955644715b0781a1621fe8bb129
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-02 18:17:18 -0500

    Remove extra divs and make classnames specific

--- src/components/PhotoAdd/PhotoAdd.js
+++ src/components/PhotoAdd/PhotoAdd.js
@@ -8,23 +8,17 @@ const PhotoAdd = () => {
 
     return (
     <section className='photo-add'>
-        <div className='small-square corner1'></div>
-        <div className='rectangle'></div>
-        <div className='small-square middle1'></div>
-        <div className='rectangle'></div>
-        <div className='small-square corner2'></div>
-        <div className='rectangle'></div>
-        <div className='rectangle'></div>
-        <div className='small-square middle2'></div>
-        <div className='small-square middle3'></div>
-        <div className='rectangle'></div>
-        <div className='rectangle'></div>
-        <div className='small-square corner3'></div>
-        <button className='edit-button rectangle' onClick={() => handleSubmit()}>Submit</button>
-        <div className='small-square middle4'></div>
-        <div className='rectangle'></div>
-        <div className='small-square corner4'></div>
-        {/* {error && <div className='obit-error'>{error}!</div>} */}
+        <div className="photo-small-square photo-corner1"></div>
+        <div className="photo-title"><h2>Edit Profile Picture</h2></div>
+        <div className="photo-small-square photo-corner2"></div>
+        <div className="photo-rectangle side1"></div>
+        <div className="photo-rectangle side2"></div>
+        <div className="photo-rectangle"></div>
+        <div className="photo-rectangle"></div>
+        <div className="photo-small-square photo-corner3"></div>
+        <button className='edit-button photo-rectangle bottom-rectangle1' onClick={() => handleSubmit()}>Submit</button>
+        <div className="photo-rectangle bottom-rectangle2"></div>
+        <div className="photo-small-square photo-corner4"></div>
         <article className="file-form"><input type='file'></input></article>
     </section>
     )
