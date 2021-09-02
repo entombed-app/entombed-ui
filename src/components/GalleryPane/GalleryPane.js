6610f9f11d55c3b002e645fd989cbe4c614a365b
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-02 17:08:06 -0600

    Add structural styling

--- src/components/GalleryPane/GalleryPane.js
+++ src/components/GalleryPane/GalleryPane.js
@@ -3,7 +3,19 @@ import "./GalleryPane.css"
 
 export const GalleryPane = props => {
   return (
-    <>
-    </>
+    <section class="gallery">
+      <section class="profile-dome">
+        <div class="left-dome"></div>
+        <div class="center-dome">
+          <img src="https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__480.jpg" class="profile-dome"/>
+        </div>
+        <div class="right-dome"></div>
+      </section>
+      <section class="slideshow">
+        <h4 class="slideshow-header">Slideshow</h4>
+      </section>
+      <section class="gal-view">
+      </section>
+  </section>
   )
 }
