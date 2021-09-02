d40a8f6002f5820d33d8317175da3b06c385cdda
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-02 17:12:43 -0600

    Build slideshow header and add a photo button

--- src/components/GalleryPane/GalleryPane.js
+++ src/components/GalleryPane/GalleryPane.js
@@ -3,14 +3,18 @@ import "./GalleryPane.css"
 
 export const GalleryPane = props => {
   return (
-    <section class="gallery">
-      <section class="profile-dome">
-        <div class="left-dome"></div>
-        <div class="center-dome">
-          <img src="https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__480.jpg" class="profile-dome"/>
+    <section className="gallery">
+      <section className="profile-dome">
+        <div className="left-dome"></div>
+        <div className="center-dome">
+          <img src="https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__480.jpg" className="profile-dome"/>
         </div>
-        <div class="right-dome"></div>
+        <div className="right-dome"></div>
       </section>
-  </section>
+      <section className="slideshow">
+      <h4 className="slideshow-header">Slideshow</h4>
+      <button className="add-gal-photo">Add a photo</button>
+      </section>
+    </section>
   )
 }
