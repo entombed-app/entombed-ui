3844ff3035ae9b80394cb91a457cdc626696ccac
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-02 17:59:28 -0600

    Format gallery view to hide overflow of photo grid

--- src/components/GalleryPane/GalleryPane.js
+++ src/components/GalleryPane/GalleryPane.js
@@ -12,8 +12,36 @@ export const GalleryPane = props => {
         <div className="right-dome"></div>
       </section>
       <section className="slideshow">
-      <h4 className="slideshow-header">Slideshow</h4>
-      <button className="add-gal-photo">Add a photo</button>
+        <h4 className="slideshow-header">Slideshow</h4>
+        <button className="add-gal-photo">Add a photo</button>
+      </section>
+      <section className="gal-view">
+        <section className="gallery-panes">
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond">
+            <img className="gal-img" src="http://c.files.bbci.co.uk/11382/production/_119803507_mediaitem119803506.jpg"/>
+          </div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond">
+            <img className="gal-img" src="https://cdn.mos.cms.futurecdn.net/RY2EpSo74hvYXyAVpTN2Gg.jpg"/>
+          </div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+          <div className="gal-diamond"></div>
+        </section>
       </section>
     </section>
   )
