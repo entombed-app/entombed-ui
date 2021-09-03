2d5cd4444f264240bae23c97670cc5c0bf6854f0
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-02 20:01:39 -0600

    Add preliminary tests for gallery view

--- cypress/integration/gallery_spec.js
+++ cypress/integration/gallery_spec.js
@@ -8,4 +8,43 @@ describe('Gallery', () => {
     })
     cy.visit("http://localhost:3000/")
   })
+
+  it('Should be able to visit the gallery page by typing in the path in the url', () => {
+    cy.get(".gallery-pane")
+      .click()
+    cy.url().should("include", "http://localhost:3000/gallery")
+  })
+
+  it('Should display the user profile photo in the center pane', () => {
+    cy.get(".gallery-pane")
+      .click()
+      .get(".gal-prof")
+      .should("be.visible")
+  })
+
+  it('Should have a button for the user to add a photo', () => {
+    cy.get(".gallery-pane")
+      .click()
+      .get(".add-gal-photo")
+      .should("be.visible")
+  })
+
+  it.skip('Should redirect user to add photo view', () => {
+    cy.get(".gallery-pane")
+      .click()
+      .get(".add-gal-photo")
+      .click()
+      cy.url().should("include", "http://localhost:3000/add-photo")
+  })
+
+  it('Should display the user photos in the panes of the window', () => {
+    cy.get(".gallery-pane")
+      .click()
+      .get(".g-10")
+      .should("include", "img")
+      .get(".g-17")
+      .should("include", "img")
+      .get(".g-9")
+      .should("include", "img")
+  })
 });
