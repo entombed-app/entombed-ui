1eeb5aeb8b08dfa369fb011fd758be6fe61f16fa
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-02 20:11:55 -0500

    Update URL for tests

--- cypress/integration/obituary_spec.js
+++ cypress/integration/obituary_spec.js
@@ -1,9 +1,9 @@
 describe('Obituary', () => {
     beforeEach(() => {
-      cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/user/1", {
+      cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/2", {
           ok: true,
           status: 200, 
-          url: "https://elegy-backend.herokuapp.com/api/v1/user/1",
+          url: "https://elegy-backend.herokuapp.com/api/v1/users/2",
           fixture: 'user'
       })
       cy.visit("http://localhost:3000/obituary")
