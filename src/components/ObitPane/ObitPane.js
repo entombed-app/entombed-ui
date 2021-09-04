ac9354f0c46d1883d3fe74ea2259abed474eb5c4
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-04 13:57:14 -0600

    Restyle obit view

--- src/components/ObitPane/ObitPane.js
+++ src/components/ObitPane/ObitPane.js
@@ -27,43 +27,46 @@ const ObitPane = ({obit}) => {
     }
     
     return (
-        <section className='obit'>
-            <div className='small-square corner1'></div>
-            <div className='rectangle'></div>
-            <div className='small-square middle1'></div>
-            <div className='rectangle'></div>
-            <div className='small-square corner2'></div>
-            <div className='rectangle'></div>
-            <div className='rectangle'></div>
-            <div className='small-square middle2'></div>
-            <div className='small-square middle3'></div>
-            <div className='rectangle'></div>
-            <div className='rectangle'></div>
-            <div className='small-square corner3'></div>
-            {editMode 
-                ? <button className='edit-button rectangle' onClick={() => handleSubmit()}>Submit</button>
-                : <button className='edit-button rectangle' onClick={() => handleClickEdit()}>Edit</button>
-            }
-            <div className='small-square middle4'></div>
-            {editMode
-                ? <div className='rectangle'><p className='remaining'>Limit: {usedChars}/500</p></div> 
-                : <div className='rectangle'></div>
-            }
-            <div className='small-square corner4'></div>
-            {error && <div className='obit-error'>{error}!</div>}
-            {editMode 
-                ?   <textarea 
-                        className='obit-text'
-                        placeholder='How do you want to be remembered?'
-                        value={obituary}
-                        onChange={(e) => {handleChange(e)}}
-                        maxLength='500'
-                    >
-                    </textarea>   
-                    
-                : <article className='obit-text'>{obituary}</article>
-            }
-        </section>
+        <>
+            <section className="obit-head"><h2>Obituary</h2></section>
+            <section className='obit'>
+                <div className='small-square corner1'></div>
+                <div className='rectangle'></div>
+                <div className='small-square middle1'></div>
+                <div className='rectangle'></div>
+                <div className='small-square corner2'></div>
+                <div className='rectangle'></div>
+                <div className='rectangle'></div>
+                <div className='small-square middle2'></div>
+                <div className='small-square middle3'></div>
+                <div className='rectangle'></div>
+                <div className='rectangle'></div>
+                <div className='small-square corner3'></div>
+                {editMode 
+                    ? <button className='edit-button rectangle' onClick={() => handleSubmit()}>Submit</button>
+                    : <button className='edit-button rectangle' onClick={() => handleClickEdit()}>Edit</button>
+                }
+                <div className='small-square middle4'></div>
+                {editMode
+                    ? <div className='rectangle'><p className='remaining'>Limit: {usedChars}/500</p></div> 
+                    : <div className='rectangle'></div>
+                }
+                <div className='small-square corner4'></div>
+                {error && <div className='obit-error'>{error}!</div>}
+                {editMode 
+                    ?   <textarea 
+                            className='obit-text'
+                            placeholder='How do you want to be remembered?'
+                            value={obituary}
+                            onChange={(e) => {handleChange(e)}}
+                            maxLength='500'
+                        >
+                        </textarea>   
+                        
+                    : <article className='obit-text'>{obituary}</article>
+                }
+            </section>
+        </>
     )
 }
 
