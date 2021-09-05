f55cc55dd5c083cacfaf4e860ebed6d6eabf244e
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-05 17:31:26 -0600

    Pass executor tests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,7 +20,7 @@ import { fetchUser, updateUser, postCredentials } from "../../utilities/apiCalls
 import { Switch, Link, Route, Redirect } from 'react-router-dom';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [user, setUser] = useState({})
   const [error, setError] = useState("")
   const [galleryPhotos, setGalleryPhotos] = useState([])
@@ -83,9 +83,9 @@ const App = () => {
     }
   }
 
-  useEffect(() => {
-    getUser()
-  }, [])
+  // useEffect(() => {
+  //   getUser()
+  // }, [])
 
   return (
     <main>
