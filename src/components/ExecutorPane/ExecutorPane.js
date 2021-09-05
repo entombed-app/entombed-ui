62db9b8898950594d450c5248664c9a5cf6f2168
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-05 14:59:56 -0600

    Remove unused styling

--- src/components/ExecutorPane/ExecutorPane.js
+++ src/components/ExecutorPane/ExecutorPane.js
@@ -7,32 +7,6 @@ export const ExecutorPane = ({executors}) => {
       <section className="exec-border">
         {/* <h2 className="exec-title">Executor</h2> */}
         <section className="exec-window">
-          {/* <div className="shape one">
-          </div>
-          <div className="line l-one">
-          </div>
-          <div className="shape two">
-          </div>
-          <div className="line l-two">
-          </div>
-          <div className="shape three">
-          </div>
-          <div className="line l-three">
-          </div>
-          <div className="shape four">
-          </div>
-          <div className="line l-four">
-          </div>
-          <div className="shape five">
-          </div>
-          <div className="line l-five">
-          </div>
-          <div className="shape six">
-          </div>
-          <div className="shape seven">
-          </div>
-          <div className="line l-six">
-          </div> */}
           <div className="top">
             <div className="top-left box"></div>
             <div className="bottom-left box"></div>
@@ -51,11 +25,6 @@ export const ExecutorPane = ({executors}) => {
             <h4 className="exec-email">{executors[0].attributes.email}</h4>
         </section>
         </section>
-        {/* <section className="exec-details">
-          <h2 className="exec-name">{executors[0].attributes.name}</h2>
-          <h3 data-cy="exec-phone">{executors[0].attributes.phone}</h3>
-          <h4 className="exec-email">{executors[0].attributes.email}</h4>
-        </section> */}
       </section>
     </section>
   )
