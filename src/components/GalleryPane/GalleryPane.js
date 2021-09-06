4829403f866ba6c6565af731de3a206d83ebe3c9
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-05 20:55:22 -0600

    Remove console.logs

--- src/components/GalleryPane/GalleryPane.js
+++ src/components/GalleryPane/GalleryPane.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom"
 import "./GalleryPane.css"
 
 export const GalleryPane = ({ galPhotos, profPhoto }) => {
-  console.log(profPhoto)
 
   return (
     <section className="gallery">
