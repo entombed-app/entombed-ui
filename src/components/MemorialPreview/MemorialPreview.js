b4b9a6b2918b01cc57f42c4c5638082461d4278c
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-05 18:49:02 -0600

    Add structure to memorial preview component

---
+++ src/components/MemorialPreview/MemorialPreview.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react';
+import './MemorialPreview.css';
+
+export const MemorialPreview = () => {
+  return (
+    <section className="preview-pane">
+      <section className="prev-head">
+
+      </section>
+      <aside className="prev-exec">
+
+      </aside>
+      <article className="prev-obit">
+
+      </article>
+      
+    </section>
+  )
+}
