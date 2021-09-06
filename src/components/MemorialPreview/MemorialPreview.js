75c7e7992ee5d0f5dc1f47c4f9e863e1b63724d3
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-09-05 19:17:54 -0600

    Add rays to header

--- src/components/MemorialPreview/MemorialPreview.js
+++ src/components/MemorialPreview/MemorialPreview.js
@@ -2,11 +2,20 @@ import React, {useEffect, useState} from 'react';
 import './MemorialPreview.css';
 
 
-export const MemorialPreview = ({name, dob, etd, executors, obit, profPhoto, galPhotos, }) => {
+export const MemorialPreview = ({name, isLoggedIn, dob, etd, executors, obit, profPhoto, galPhotos, }) => {
   return (
-    <section className="preview-pane">
+    <section className="preview">
       <section className="prev-head">
-
+        <div className="rays">
+          <div className="ray ray2"></div>
+          <div className="ray ray3"></div>
+          <div className="ray ray4"></div>
+          <div className="ray ray5"></div>
+          <div className="ray ray6"></div>
+        </div>
+        <div className="mem-photo">
+          {/* <img src={profPhoto}/> */}
+        </div>
       </section>
       <aside className="prev-exec">
 
