d242dcd9b74f35a120938f038b719a7fb81948bd
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-06 19:02:20 -0500

    Add command to intercept all fetches

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,3 +8,27 @@ Cypress.Commands.add("login", () => {
   cy.get(".submit-login")
   .click()
 })
+
+Cypress.Commands.add("interceptFetch", () => {
+  cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4", {
+    ok: true,
+    status: 200, 
+    url: "https://elegy-backend.herokuapp.com/api/v1/users/4",
+    fixture: 'user'
+  }).as("getUser")
+
+  cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4/executors", {
+    ok: true,
+    status: 200, 
+    url: "https://elegy-backend.herokuapp.com/api/v1/users/4",
+    fixture: 'executors'
+  }).as("getExecutors")
+
+  cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4", {
+    ok: true,
+    status: 200, 
+    url: "https://elegy-backend.herokuapp.com/api/v1/users/4/recipients",
+    fixture: 'recipients'
+  }).as("getRecipients")
+
+})
