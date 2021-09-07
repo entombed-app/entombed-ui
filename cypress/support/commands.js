6e594e8b657bc33a001ae96fd71e056185849056
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-06 19:54:49 -0500

    Fix fail command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,24 +12,56 @@ Cypress.Commands.add("login", () => {
 Cypress.Commands.add("interceptGets", () => {
   cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4", {
     ok: true,
-    status: 200, 
+    statusCode: 200, 
     url: "https://elegy-backend.herokuapp.com/api/v1/users/4",
     fixture: 'user'
   }).as("getUser")
 
   cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4/executors", {
     ok: true,
-    status: 200, 
+    statusCode: 200, 
     url: "https://elegy-backend.herokuapp.com/api/v1/users/4",
     fixture: 'executors'
   }).as("getExecutors")
 
   cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4/recipients", {
     ok: true,
-    status: 200, 
+    statusCode: 200, 
     url: "https://elegy-backend.herokuapp.com/api/v1/users/4/recipients",
     fixture: 'recipients'
   }).as("getRecipients")
+
+  cy.intercept("POST", 'https://elegy-backend.herokuapp.com/api/v1/login', 
+  {
+    ok: true,
+    statusCode: 201,
+    url: "https://elegy-backend.herokuapp.com/api/v1/users/2",
+    body: {
+      data: {
+        fixture: 'user'
+      }
+    }
+  }).as("loginPost")
+})
+
+Cypress.Commands.add("interceptFails", () => {
+  cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4", {
+    ok: false,
+    statusCode: 404, 
+    url: "https://elegy-backend.herokuapp.com/api/v1/users/4",
+  }).as("getUserFail")
+
+  cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4/executors", {
+    ok: false,
+    statusCode: 404, 
+    url: "https://elegy-backend.herokuapp.com/api/v1/users/4",
+  }).as("getExecutorsFail")
+
+  cy.intercept("GET", "https://elegy-backend.herokuapp.com/api/v1/users/4/recipients", {
+    ok: false,
+    statusCode: 404, 
+    url: "https://elegy-backend.herokuapp.com/api/v1/users/4/recipients",
+  }).as("getRecipientsFail")
 
   cy.intercept("POST", 'https://elegy-backend.herokuapp.com/api/v1/login', 
   {
