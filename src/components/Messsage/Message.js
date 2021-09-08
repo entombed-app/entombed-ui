faf1483fb8305920a46e6b13dea72feb3468cf01
Taylor Galloway <gallowaytaylor@outlook.com>
2021-09-07 19:02:34 -0500

    Padd logout through message component

--- src/components/Messsage/Message.js
+++ src/components/Messsage/Message.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Header from "../Header/Header";
 
-const Message = ({error, profilePic}) => {
+const Message = ({error, profilePic, logOut}) => {
     let message;
     !error ? message = "Loading..." : message = error 
     return (
         <>
-            {error !== "404 page not found. Click title above." && <Header profilePic={profilePic}/>} 
+            {error !== "404 page not found. Click title above." && <Header profilePic={profilePic} logOut={logOut}/>} 
             <p className='loading-error-message'>{message}</p>
         </>
     )
