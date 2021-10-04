a17cf90c307c79e3a704fe48391193d9654cd706
Taylor Galloway <gallowaytaylor@outlook.com>
2021-10-04 17:41:08 -0500

    Make executor pane dynamic for rendering recipients

--- src/components/ExecutorPane/ExecutorPane.js
+++ src/components/ExecutorPane/ExecutorPane.js
@@ -1,11 +1,27 @@
 import React from "react"
 import "./ExecutorPane.css"
 
-export const ExecutorPane = ({executors}) => {
+export const ExecutorPane = ({person, type}) => {
+  let details;
+  if (type) {
+    details = 
+      <section className="exec-details">
+        <h2 className="exec-name">{person[0].attributes.name}</h2>
+        <h3 className="exec-phone" data-cy="exec-phone">{person[0].attributes.phone}</h3>
+        <h4 className="exec-email">{person[0].attributes.email}</h4>
+      </section>
+  } else {
+    details = 
+      <section className="exec-details">
+        <h2 className="exec-name">{person.name}</h2>
+        <h4 className="exec-email">{person.email}</h4>
+      </section>  
+  }
+
   return (
     <section className="exec-pane">
       <section className="exec-border">
-        <h2 className="exec-title">Executor</h2>
+        <h2 className="exec-title">{type ? 'Recipient' : 'Executor'}</h2>
         <section className="exec-window">
           <div className="top">
             <div className="top-left box"></div>
@@ -19,11 +35,12 @@ export const ExecutorPane = ({executors}) => {
           </div>
           <div className="inner-circle">
           </div>
-          <section className="exec-details">
-            <h2 className="exec-name">{executors[0].attributes.name}</h2>
-            <h3 className="exec-phone" data-cy="exec-phone">{executors[0].attributes.phone}</h3>
-            <h4 className="exec-email">{executors[0].attributes.email}</h4>
-        </section>
+          {details}
+          {/* <section className="exec-details">
+            <h2 className="exec-name">{person[0].attributes.name}</h2>
+            <h3 className="exec-phone" data-cy="exec-phone">{person[0].attributes.phone}</h3>
+            <h4 className="exec-email">{person[0].attributes.email}</h4>
+          </section> */}
         </section>
       </section>
     </section>
