8faf8b2499a275015b3d35446f662e73dddf1c99
Taylor Galloway <gallowaytaylor@outlook.com>
2021-10-06 15:45:42 -0500

    Finish controlled form for new recipient

--- src/components/RecipientsPane/RecipientsPane.js
+++ src/components/RecipientsPane/RecipientsPane.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import { ExecutorPane } from "../ExecutorPane/ExecutorPane";
 import "./RecipientsPane.css"
 
-const RecipientsPane = ({recipients, handleDeleteRecipient}) => {
+const RecipientsPane = ({recipients, handleDeleteRecipient, handleCreateRecipient}) => {
+    const [newName, setNewName] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+
     const allRecipients = recipients.map(recipient => <ExecutorPane person={recipient.attributes} id={recipient.id} type="recipient" handleDeleteRecipient={handleDeleteRecipient} key={recipient.id}/>)
+
+    const handleClick = async () => {
+        handleCreateRecipient({name: newName, email: newEmail})
+    }
 
     return (
         <section className="recipients-pane">
@@ -12,10 +19,10 @@ const RecipientsPane = ({recipients, handleDeleteRecipient}) => {
                     <form className="recipients-form">
                         <h2 className="exec-title">Add New Recipient</h2>
                         <label>Name</label>
-                        <input type="text"></input>
+                        <input type="text" placeholder="Name" value={newName} onChange={(e)=> setNewName(e.target.value)}></input>
                         <label>Email</label>
-                        <input type="email"></input>
-                        <button className="add-new-recipient-button">Create</button>
+                        <input type="email" placeholder="Email" value={newEmail} onChange={(e)=> setNewEmail(e.target.value)}></input>
+                        <button className="add-new-recipient-button" onClick={() => handleClick()}>Create</button>
                     </form>
             </section>
         </section>
