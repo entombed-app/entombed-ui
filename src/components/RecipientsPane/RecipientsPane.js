d14eee77699ecec2ff422b60a46cf0e4da8d7c6a
Taylor Galloway <gallowaytaylor@outlook.com>
2021-10-06 15:26:35 -0500

    Add form and styling for new recipient

--- src/components/RecipientsPane/RecipientsPane.js
+++ src/components/RecipientsPane/RecipientsPane.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ExecutorPane } from "../ExecutorPane/ExecutorPane";
+import "./RecipientsPane.css"
 
 const RecipientsPane = ({recipients, handleDeleteRecipient}) => {
     const allRecipients = recipients.map(recipient => <ExecutorPane person={recipient.attributes} id={recipient.id} type="recipient" handleDeleteRecipient={handleDeleteRecipient} key={recipient.id}/>)
@@ -7,6 +8,16 @@ const RecipientsPane = ({recipients, handleDeleteRecipient}) => {
     return (
         <section className="recipients-pane">
             {allRecipients}
+            <section className="add-recipients-pane">
+                    <form className="recipients-form">
+                        <h2 className="exec-title">Add New Recipient</h2>
+                        <label>Name</label>
+                        <input type="text"></input>
+                        <label>Email</label>
+                        <input type="email"></input>
+                        <button className="add-new-recipient-button">Create</button>
+                    </form>
+            </section>
         </section>
     )
 }
