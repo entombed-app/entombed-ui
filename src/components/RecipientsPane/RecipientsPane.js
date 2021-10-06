285f5bb3e23618a97d8eeea82056d890e7de45ea
Taylor Galloway <gallowaytaylor@outlook.com>
2021-10-06 13:17:17 -0500

    Add key to each recipient

--- src/components/RecipientsPane/RecipientsPane.js
+++ src/components/RecipientsPane/RecipientsPane.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { ExecutorPane } from '../ExecutorPane/ExecutorPane';
+import React from "react";
+import { ExecutorPane } from "../ExecutorPane/ExecutorPane";
 
 const RecipientsPane = ({recipients, handleDeleteRecipient}) => {
-    const allRecipients = recipients.map(recipient => <ExecutorPane person={recipient.attributes} id={recipient.id} type='recipient' handleDeleteRecipient={handleDeleteRecipient}/>)
+    const allRecipients = recipients.map(recipient => <ExecutorPane person={recipient.attributes} id={recipient.id} type="recipient" handleDeleteRecipient={handleDeleteRecipient} key={recipient.id}/>)
 
     return (
-        <section className='recipients-pane'>
+        <section className="recipients-pane">
             {allRecipients}
         </section>
     )
