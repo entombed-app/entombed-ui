c501dbd9cbc584df4ecc02c236fa373a628ec706
Taylor Galloway <gallowaytaylor@outlook.com>
2021-10-07 16:47:54 -0500

    Fix date format for submission

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -117,6 +117,7 @@ const App = () => {
   }
 
   const updateETD = async (newDate) => {
+    console.log(newDate)
     try {
       const updatedUser = {...user}
       updatedUser.attributes.user_etd = newDate
