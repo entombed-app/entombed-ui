333079d46d4ff08e36564376ced3e2cc0e326462
Taylor Galloway <gallowaytaylor@outlook.com>
2021-11-12 12:32:49 -0600

    Add cleaning function to add s to url protocol

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -3,3 +3,16 @@ export const restructureDate = (originalDate) => {
     let [year, month, day] = separateDates;
     return `${month}-${day}-${year}`;
   }
+
+export const secureImages = (userData) => {
+  const urls = userData.data.attributes.images_urls.map(url => {
+    if (url.slice(0, 5) === 'http:') {
+      return`https${url.slice(4, url.length)}`
+    }
+    return url
+  })
+  const profile_url = userData.data.attributes.profile_picture_url
+  userData.data.attributes.profile_picture_url = `https${profile_url.slice(4, profile_url.length)}`
+  userData.data.attributes.images_urls = urls
+  return userData
+}  
